e185c6df285b131f962a355117d0dc99a3ffa873
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-12-26 16:48:46 -0300

    Iniciando a utilização do provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop/pages/counter_page.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -9,24 +10,27 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple, secondary: Colors.deepOrange),
-        fontFamily: 'Lato',
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple, secondary: Colors.deepOrange),
+          fontFamily: 'Lato',
+          useMaterial3: true,
+        ),
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.productDetail: (ctx) => const ProductDetailPage()
-      },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
