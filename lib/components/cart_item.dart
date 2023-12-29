38d8a5a24529b3157274c0789992c34c6140c93e
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2023-12-28 22:24:31 -0300

    incluindo descrições para tela de detalhes

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,5 +1,3 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -2,18 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
 
 class ProductDetailPage extends StatelessWidget {
- 
   const ProductDetailPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-  final Product product = ModalRoute.of(context)!.settings.arguments as Product;
+    final Product product =
+        ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(product.name,
-        style: const TextStyle(color: Colors.white),
+        title: Text(
+          product.name,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'R\$ ${product.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
         ),
       ),
     );
